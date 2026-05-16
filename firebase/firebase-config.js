3a1864491d3df7d61218d05a72b19a8dad8a1f8a
joabeplay1 <joabejonatas186@gmail.com>
2026-05-16 19:04:31 -0400

    firebase-config.js
    Added Firebase configuration with placeholder values.

---
+++ firebase/firebase-config.js
@@ -0,0 +1,10 @@
+// Substitua pelas suas chaves quando for conectar o banco
+export const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "seu-projeto.firebaseapp.com",
+    databaseURL: "https://seu-projeto-default-rtdb.firebaseio.com",
+    projectId: "seu-projeto",
+    storageBucket: "seu-projeto.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
